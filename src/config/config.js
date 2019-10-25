687392eb4297d940a1c4aa7a4fc1712ffa5c4ae1
Hagop Jamkojian <hagopj13@gmail.com>
2019-10-26 01:17:38 +0200

    Use environment variables

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+  path: path.join(__dirname, '../../.env'),
+});
+
+module.exports = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
+const { port } = require('./config/config');
 
-const port = 3000;
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
