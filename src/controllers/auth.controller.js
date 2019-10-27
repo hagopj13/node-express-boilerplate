39b34a8d43429ac5f1ed96a50d5b8ea0f585ea2b
Hagop Jamkojian <hagopj13@gmail.com>
2019-10-27 19:59:13 +0100

    Wrap controllers around catchAsync

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,13 +1,14 @@
 const httpStatus = require('http-status');
+const catchAsync = require('../utils/catchAsync');
 const { userService } = require('../services');
 
-const register = async (req, res) => {
+const register = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
   const response = {
     user: user.transform(),
   };
   res.status(httpStatus.CREATED).send(response);
-};
+});
 
 module.exports = {
   register,
---
+++ src/utils/catchAsync.js
@@ -0,0 +1,5 @@
+const catchAsync = fn => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(err => next(err));
+};
+
+module.exports = catchAsync;
