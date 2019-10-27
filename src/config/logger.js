c835a9791ca9b016256a0a237ecda9c71ac417ed
Hagop Jamkojian <hagopj13@gmail.com>
2019-10-27 14:35:18 +0100

    Use pm2 to start the app in production mode

--- src/config/logger.js
+++ src/config/logger.js
@@ -5,7 +5,6 @@ const logger = winston.createLogger({
   level: config.env === 'development' ? 'debug' : 'info',
   transports: [
     new winston.transports.Console({
-      level: 'debug',
       stderrLevels: ['error'],
       format: winston.format.combine(
         config.env === 'development' ? winston.format.colorize() : winston.format.uncolorize(),
