0f0b56bb8f7c44fcd49ba3e3d653ab4732312528
Hagop Jamkojian <hagopj13@gmail.com>
2019-11-02 22:53:43 +0100

    Configure jest options for coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   testEnvironment: 'node',
+  testEnvironmentOptions: {
+    NODE_ENV: 'test',
+  },
   restoreMocks: true,
+  coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests'],
+  coverageReporters: ['text', 'lcov', 'clover', 'html'],
 };
