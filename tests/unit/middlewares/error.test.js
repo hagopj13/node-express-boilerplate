7ce299777e04d064805a42dae35b2e591f647ead
Hagop Jamkojian <hagopj13@gmail.com>
2019-11-03 17:19:30 +0100

    Write error handler tests

--- tests/unit/middlewares/error.test.js
+++ tests/unit/middlewares/error.test.js
@@ -1,14 +1,18 @@
 const httpStatus = require('http-status');
 const httpMocks = require('node-mocks-http');
-const { errorConverter } = require('../../../src/middlewares/error');
+const { errorConverter, errorHandler } = require('../../../src/middlewares/error');
 const AppError = require('../../../src/utils/AppError');
+const config = require('../../../src/config/config');
+const logger = require('../../../src/config/logger');
 
 describe('Error middlewares', () => {
   describe('Error converter', () => {
     test('should return the same AppError object it was called with', () => {
       const error = new AppError(httpStatus.BAD_REQUEST, 'Any error');
       const next = jest.fn();
+
       errorConverter(error, httpMocks.createRequest(), httpMocks.createResponse, next);
+
       expect(next).toHaveBeenCalledWith(error);
     });
 
@@ -16,7 +20,9 @@ describe('Error middlewares', () => {
       const error = new Error('Any error');
       error.statusCode = httpStatus.BAD_REQUEST;
       const next = jest.fn();
+
       errorConverter(error, httpMocks.createRequest(), httpMocks.createResponse, next);
+
       expect(next).toHaveBeenCalledWith(expect.any(AppError));
       expect(next).toHaveBeenCalledWith(
         expect.objectContaining({
@@ -30,7 +36,9 @@ describe('Error middlewares', () => {
     test('should convert an Error without status to AppError with status 500', () => {
       const error = new Error('Any error');
       const next = jest.fn();
+
       errorConverter(error, httpMocks.createRequest(), httpMocks.createResponse, next);
+
       expect(next).toHaveBeenCalledWith(expect.any(AppError));
       expect(next).toHaveBeenCalledWith(
         expect.objectContaining({
@@ -45,7 +53,9 @@ describe('Error middlewares', () => {
       const error = new Error();
       error.statusCode = httpStatus.BAD_REQUEST;
       const next = jest.fn();
+
       errorConverter(error, httpMocks.createRequest(), httpMocks.createResponse, next);
+
       expect(next).toHaveBeenCalledWith(expect.any(AppError));
       expect(next).toHaveBeenCalledWith(
         expect.objectContaining({
@@ -59,7 +69,9 @@ describe('Error middlewares', () => {
     test('should convert any other object to AppError with status 500 and its message', () => {
       const error = {};
       const next = jest.fn();
+
       errorConverter(error, httpMocks.createRequest(), httpMocks.createResponse, next);
+
       expect(next).toHaveBeenCalledWith(expect.any(AppError));
       expect(next).toHaveBeenCalledWith(
         expect.objectContaining({
@@ -68,6 +80,72 @@ describe('Error middlewares', () => {
           isOperational: false,
         })
       );
+    });
+  });
+
+  describe('Error handler', () => {
+    beforeEach(() => {
+      jest.spyOn(logger, 'error').mockImplementation(() => {});
+    });
+
+    test('should send proper error response and put the error message in res.locals', () => {
+      const error = new AppError(httpStatus.BAD_REQUEST, 'Any error');
+      const res = httpMocks.createResponse();
+      const sendSpy = jest.spyOn(res, 'send');
+
+      errorHandler(error, httpMocks.createRequest(), res);
+
+      expect(sendSpy).toHaveBeenCalledWith(expect.objectContaining({ code: error.statusCode, message: error.message }));
+      expect(res.locals.errorMessage).toBe(error.message);
+    });
+
+    test('should put the error stack in the response if in development mode', () => {
+      config.env = 'development';
+      const error = new AppError(httpStatus.BAD_REQUEST, 'Any error');
+      const res = httpMocks.createResponse();
+      const sendSpy = jest.spyOn(res, 'send');
+
+      errorHandler(error, httpMocks.createRequest(), res);
+
+      expect(sendSpy).toHaveBeenCalledWith(
+        expect.objectContaining({ code: error.statusCode, message: error.message, stack: error.stack })
+      );
+      config.env = process.env.NODE_ENV;
+    });
+
+    test('should send internal server error status and message if in production mode and error is not operational', () => {
+      config.env = 'production';
+      const error = new AppError(httpStatus.BAD_REQUEST, 'Any error', false);
+      const res = httpMocks.createResponse();
+      const sendSpy = jest.spyOn(res, 'send');
+
+      errorHandler(error, httpMocks.createRequest(), res);
+
+      expect(sendSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          code: httpStatus.INTERNAL_SERVER_ERROR,
+          message: httpStatus[httpStatus.INTERNAL_SERVER_ERROR],
+        })
+      );
+      expect(res.locals.errorMessage).toBe(error.message);
+      config.env = process.env.NODE_ENV;
+    });
+
+    test('should preserve original error status and message if in production mode and error is operational', () => {
+      config.env = 'production';
+      const error = new AppError(httpStatus.BAD_REQUEST, 'Any error');
+      const res = httpMocks.createResponse();
+      const sendSpy = jest.spyOn(res, 'send');
+
+      errorHandler(error, httpMocks.createRequest(), res);
+
+      expect(sendSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          code: error.statusCode,
+          message: error.message,
+        })
+      );
+      config.env = process.env.NODE_ENV;
     });
   });
 });
