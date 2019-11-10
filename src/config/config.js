e559c130bbdb41b58c13c1628fe4ae8868e711a5
Hagop Jamkojian <hagopj13@gmail.com>
2019-11-10 14:53:32 +0100

    Add validation on the env variables config

--- src/config/config.js
+++ src/config/config.js
@@ -1,20 +1,46 @@
 const dotenv = require('dotenv');
 const path = require('path');
+const Joi = require('@hapi/joi');
 
-dotenv.config({
-  path: path.join(__dirname, '../../.env'),
-});
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+const envVarsSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string()
+      .valid('production', 'development', 'test')
+      .required(),
+    PORT: Joi.number().default(3000),
+    MONGODB_URL: Joi.string()
+      .required()
+      .description('Mongo DB url'),
+    JWT_SECRET: Joi.string()
+      .required()
+      .description('JWT secret key'),
+    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number()
+      .default(30)
+      .description('minutes after which access tokens expire'),
+    JWT_REFRESH_EXPIRATION_DAYS: Joi.number()
+      .default(30)
+      .description('days after which refresh tokens expire'),
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
 
 module.exports = {
-  env: process.env.NODE_ENV,
-  port: process.env.PORT,
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
   mongoose: {
-    url: process.env.MONGODB_URL,
+    url: envVars.MONGODB_URL,
     options: { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true },
   },
   jwt: {
-    secret: process.env.JWT_SECRET,
-    accessExpirationMinutes: parseInt(process.env.JWT_ACCESS_EXPIRATION_MINUTES, 10),
-    refreshExpirationDays: parseInt(process.env.JWT_REFRESH_EXPIRATION_DAYS, 10),
+    secret: envVars.JWT_SECRET,
+    accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
+    refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
   },
 };
