a372f5b5e733c9646b827d01f9fc6fd59ad4c8fa
Hagop Jamkojian <hagopj13@gmail.com>
2019-11-10 15:13:03 +0100

    Fix wrong environment checking in config

--- src/config/config.js
+++ src/config/config.js
@@ -35,7 +35,7 @@ module.exports = {
   env: envVars.NODE_ENV,
   port: envVars.PORT,
   mongoose: {
-    url: envVars.MONGODB_URL + (envVarsSchema.NODE_ENV === 'test' ? '-test' : ''),
+    url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
     options: { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true },
   },
   jwt: {
