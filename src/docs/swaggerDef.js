cb2c8fe04c8f73826a23bc8eb5abbb37ac495ee5
Hagop Jamkojian <hagopj13@gmail.com>
2020-05-12 22:04:51 +0200

    Create docs route

---
+++ src/docs/swaggerDef.js
@@ -0,0 +1,21 @@
+const { version } = require('../../package.json');
+const config = require('../config/config');
+
+const swaggerDef = {
+  openapi: '3.0.0',
+  info: {
+    title: 'node-express-mongoose-boilerplate API documentation',
+    version,
+    license: {
+      name: 'MIT',
+      url: 'https://github.com/hagopj13/node-express-mongoose-boilerplate/blob/master/LICENSE',
+    },
+  },
+  servers: [
+    {
+      url: `http://localhost:${config.port}/v1`,
+    },
+  ],
+};
+
+module.exports = swaggerDef;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,42 +1,12 @@
 const express = require('express');
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
 const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
+const docsRoute = require('./docs.route');
 
 const router = express.Router();
 
 router.use('/auth', authRoute);
 router.use('/users', userRoute);
-
-// Swagger set up
-const options = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Documentation',
-      version: '1.0.0',
-      description: 'Documentation discription',
-      license: {
-        name: 'MIT',
-        url: 'https://github.com/hagopj13/node-express-mongoose-boilerplate/blob/master/LICENSE',
-      },
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000/api/v1',
-      },
-    ],
-  },
-  apis: ['*.js'],
-};
-const specs = swaggerJsdoc(options);
-router.use('/docs', swaggerUi.serve);
-router.get(
-  '/docs',
-  swaggerUi.setup(specs, {
-    explorer: true,
-  })
-);
+router.use('/docs', docsRoute);
 
 module.exports = router;
