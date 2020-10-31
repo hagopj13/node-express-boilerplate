e72ce4e926ec4210e2c0fd408c42c40a913d3bac
Mahdi Rashidi <m8rashidi@gmail.com>
2020-10-31 12:06:25 +0330

    use joi instead of @hapi/joi (#39)

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,4 +1,4 @@
-const Joi = require('@hapi/joi');
+const Joi = require('joi');
 const httpStatus = require('http-status');
 const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -1,4 +1,4 @@
-const Joi = require('@hapi/joi');
+const Joi = require('joi');
 const { password, objectId } = require('./custom.validation');
 
 const createUser = {
