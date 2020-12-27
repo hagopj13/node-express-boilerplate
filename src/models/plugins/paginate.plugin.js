e003a3ce55c68d63d09275bdf8771d4f017283dc
Hagop Jamkojian <hagopj13@gmail.com>
2020-12-27 10:43:20 +0200

    Improve populate docs

--- src/models/plugins/paginate.plugin.js
+++ src/models/plugins/paginate.plugin.js
@@ -13,8 +13,8 @@ const paginate = (schema) => {
    * Query for documents with pagination
    * @param {Object} [filter] - Mongo filter
    * @param {Object} [options] - Query options
-   * @param {string} [options.populate] - populate data fields. Hierarchy of fields should be separated by (.). Multiple sorting criteria should be separated by commas (,)
    * @param {string} [options.sortBy] - Sorting criteria using the format: sortField:(desc|asc). Multiple sorting criteria should be separated by commas (,)
+   * @param {string} [options.populate] - Populate data fields. Hierarchy of fields should be separated by (.). Multiple populating criteria should be separated by commas (,)
    * @param {number} [options.limit] - Maximum number of results per page (default = 10)
    * @param {number} [options.page] - Current page (default = 1)
    * @returns {Promise<QueryResult>}
