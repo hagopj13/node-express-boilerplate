2ad446af8e1478ee7b465c2e77918b59cc0f5902
Hagop Jamkojian <hagopj13@gmail.com>
2021-03-30 17:43:39 +0200

    Remove default values from jwt expiration configs

--- src/config/config.js
+++ src/config/config.js
@@ -45,10 +45,10 @@ module.exports = {
   },
   jwt: {
     secret: envVars.JWT_SECRET,
-    accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES || 30,
-    refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS || 30,
-    resetPasswordExpirationMinutes: envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES || 10,
-    verifyEmailExpirationMinutes: envVars.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES || 10,
+    accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
+    refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
+    resetPasswordExpirationMinutes: envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
+    verifyEmailExpirationMinutes: envVars.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES,
   },
   email: {
     smtp: {
