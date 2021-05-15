9d7f16613fa31f639a4f2e85248510b456c7df4c
Devanshu <devanshu0987@gmail.com>
2021-05-15 13:24:25 +0530

    remove tests stuff from backend

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testEnvironmentOptions: {
-    NODE_ENV: 'test',
-  },
-  restoreMocks: true,
-  coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests'],
-  coverageReporters: ['text', 'lcov', 'clover', 'html'],
-};
