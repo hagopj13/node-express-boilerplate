e99b9fff4c4ea9ff610f465f04717855e353d3cb
Devanshu <devanshu0987@gmail.com>
2021-05-21 18:23:30 +0530

    added a few untested auction routes

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,2 +1,3 @@
 module.exports.authValidation = require('./auth.validation');
 module.exports.userValidation = require('./user.validation');
+module.exports.auctionValidation = require('./user.validation');
