1d90a522c3fd61e356ff5b35a79ac74cc7facd3f
Devanshu <devanshu0987@gmail.com>
2021-05-23 10:46:00 +0530

    we will only do blockchain calls from frontend

--- src/routes/v1/auction.route.js
+++ src/routes/v1/auction.route.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const validate = require('../../middlewares/validate');
 const auctionController = require('../../controllers/auction.controller');
 
 const router = express.Router();
 
 // add validation later or something
-router.get('/', auctionController.getAll);
+router.get('/all', auctionController.getAll);
 router.post('/open', auctionController.openAuction);
 router.get('/:auctionId', auctionController.getOne);
 router.post('/:auctionId/finish', auctionController.finishAuction);
