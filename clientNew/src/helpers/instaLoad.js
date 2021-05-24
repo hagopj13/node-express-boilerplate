973decbb7d378b7a25bd6c64e458c1ceea77ac9d
Devanshu <devanshu0987@gmail.com>
2021-05-25 00:19:49 +0530

    fetch image working

--- clientNew/src/helpers/instaLoad.js
+++ clientNew/src/helpers/instaLoad.js
@@ -1,57 +1,60 @@
-const getPage = function (userId) {
-    window.FB.api(
-        `"/${userId}/accounts"`,
-        function (response) {
-            if (response && !response.error) {
-                /* handle the result */
-                // we will get the fb page id associated with instagram
-                let pageId = response.data[0].id;
-            }
-        }
-    );
-}
-
-const getInstagramId = function (pageId) {
-    window.FB.api(
-        `"/${pageId}?fields=instagram_business_account"`,
-        function (response) {
-            if (response && !response.error) {
-                /* handle the result */
-                // we will get the fb page id associated with instagram
-                let instaId = response.instagram_business_account.id;
-            }
-        }
-    );
-}
-
-const getListOfMedia = function (instaId) {
-    window.FB.api(
-        `"/${instaId}/media"`,
-        function (response) {
-            if (response && !response.error) {
-                /* handle the result */
-                // we will get the fb page id associated with instagram
-                let listOfMedia = response.data;
-                for (let index = 0; index < listOfMedia.length; index++) {
-                    let id = listOfMedia[index].id;
+export const getPage = async function (userId) {
+    return new Promise(function (resolve, reject) {
+        window.FB.api(
+            `${userId}/accounts`,
+            function (response) {
+                if (response && !response.error) {
+                    // we will get the fb page id associated with instagram
+                    resolve(response.data[0].id);
                 }
             }
-        }
-    );
+        );
+    });
 }
 
-const getMedia = function (mediaId) {
-    window.FB.api(
-        `"/${mediaId}/?fields=permalink"`,
-        function (response) {
-            if (response && !response.error) {
-                /* handle the result */
-                // we will get the fb page id associated with instagram
-                let bag = {
-                    "id": mediaId,
-                    "url": response.permalink
+export const getInstagramId = function (pageId) {
+    return new Promise(function (resolve, reject) {
+        window.FB.api(
+            `${pageId}?fields=instagram_business_account`,
+            function (response) {
+                if (response && !response.error) {
+                    // we will get the fb page id associated with instagram
+                    resolve(response.instagram_business_account.id);
                 }
             }
-        }
-    );
+        );
+    });
+}
+
+export const getListOfMedia = function (instaId) {
+    return new Promise(function (resolve, reject) {
+        window.FB.api(
+            `${instaId}/media`,
+            function (response) {
+                if (response && !response.error) {
+                    // we will get the fb page id associated with instagram
+                    // array of media
+                    resolve(response.data);
+                }
+            }
+        );
+    });
+}
+
+export const getMedia = function (mediaId) {
+    return new Promise(function (resolve, reject) {
+        window.FB.api(
+            `${mediaId}/?fields=permalink`,
+            function (response) {
+                if (response && !response.error) {
+                    // {
+                    //     "permalink": "",
+                    //     "id": ""
+                    // }
+                    resolve(response);
+                }
+            }
+        );
+    });
+
 }
--- clientNew/src/screens/Profile/Profile.jsx
+++ clientNew/src/screens/Profile/Profile.jsx
@@ -1,5 +1,23 @@
+import { getPage, getInstagramId, getListOfMedia, getMedia } from '../../helpers/instaLoad';
+import axios from 'axios';
 const Profile = () => {
-  return <>Profile</>;
+  const importImages = async function () {
+    let pageId = await getPage(window.fbid); console.log(pageId);
+    let instaId = await getInstagramId(pageId); console.log(instaId);
+    let listOfMedia = await getListOfMedia(instaId); console.log(listOfMedia);
+    let images = [];
+    for (let index = 0; index < listOfMedia.length; index++) {
+      let media = await getMedia(listOfMedia[index].id);
+      images.push(media);
+    }
+    // here we call backend and put in all images
+
+  }
+  return (
+    <div>
+      <button onClick={importImages}>Import images</button>
+    </div>
+  );
 };
 
 export default Profile;
