5466a6dfcd4c4e3dbb40ed3443f164e115a6b0f6
Hagop Jamkojian <hagopj13@gmail.com>
2021-07-04 11:42:27 +0200

    Show all validation errors instead of aborting early
    Closes #124

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -7,7 +7,7 @@ const validate = (schema) => (req, res, next) => {
   const validSchema = pick(schema, ['params', 'query', 'body']);
   const object = pick(req, Object.keys(validSchema));
   const { value, error } = Joi.compile(validSchema)
-    .prefs({ errors: { label: 'key' } })
+    .prefs({ errors: { label: 'key' }, abortEarly: false })
     .validate(object);
 
   if (error) {
