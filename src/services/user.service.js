dc5708e39e805895f37125adbaef86d280cedbf4
Hagop Jamkojian <hagopj13@gmail.com>
2021-07-04 22:07:08 +0200

    Return created user without await
    Closes #110

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -11,8 +11,7 @@ const createUser = async (userBody) => {
   if (await User.isEmailTaken(userBody.email)) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
   }
-  const user = await User.create(userBody);
-  return user;
+  return User.create(userBody);
 };
 
 /**
