ab1ccbca4a720387e34be93f7262eece9292d709
Hagop Jamkojian <hagopj13@gmail.com>
2021-07-04 11:37:03 +0200

    Fix open handle issue in test

--- tests/integration/docs.test.js
+++ tests/integration/docs.test.js
@@ -7,7 +7,7 @@ describe('Auth routes', () => {
   describe('GET /v1/docs', () => {
     test('should return 404 when running in production', async () => {
       config.env = 'production';
-      await request(app).get('/v1/docs').expect(httpStatus.NOT_FOUND);
+      await request(app).get('/v1/docs').send().expect(httpStatus.NOT_FOUND);
       config.env = process.env.NODE_ENV;
     });
   });
