c9d556fc918b4f9762b999b45b529f48e60151d9
CapsAdmin <eliashogstvedt@gmail.com>
2021-09-30 10:33:04 +0200

    jwt typings

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -2,25 +2,24 @@ import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 import { config } from './config';
 import { tokenTypes } from './tokens';
 import { User } from '../models/user.model';
-
-const jwtOptions = {
-  secretOrKey: config.jwt.secret,
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-};
-
-const jwtVerify = async (payload, done) => {
-  try {
-    if (payload.type !== tokenTypes.ACCESS) {
-      throw new Error('Invalid token type');
+import { JwtPayload } from 'jsonwebtoken';
+export const jwtStrategy = new JwtStrategy(
+  {
+    secretOrKey: config.jwt.secret,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  },
+  async (payload: JwtPayload, done) => {
+    try {
+      if (payload.type !== tokenTypes.ACCESS) {
+        throw new Error('Invalid token type');
+      }
+      const user = await User.findById(payload.sub);
+      if (!user) {
+        return done(null, false);
+      }
+      done(null, user);
+    } catch (error) {
+      done(error, false);
     }
-    const user = await User.findById(payload.sub);
-    if (!user) {
-      return done(null, false);
-    }
-    done(null, user);
-  } catch (error) {
-    done(error, false);
   }
-};
-
-export const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify);
+);
