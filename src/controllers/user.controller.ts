cbace80cac72add3b66d69df8219d46d13bc3a43
CapsAdmin <eliashogstvedt@gmail.com>
2021-09-30 09:27:36 +0200

    replace joi with zod

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -9,6 +9,7 @@ export const createUser = catchAsync(async (req, res) => {
   res.status(httpStatus.CREATED).send(user);
 });
 
+export const getUsers = catchAsync(async (req, res) => {
   const filter = pick(req.query, ['name', 'role'] as const);
   const options = pick(req.query, ['sortBy', 'limit', 'page'] as const);
   const result = await userService.queryUsers(filter, options);
