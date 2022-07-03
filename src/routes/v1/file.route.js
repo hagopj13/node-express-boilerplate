38002b92bdc5cf55bf76b8f13ce8fc75bdec9fda
Mark Thien <EX-MARKTHIEN001@ocft.com>
2022-07-03 14:57:07 +0800

    refactoring

---
+++ src/routes/v1/file.route.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const fileController = require('../../controllers/file.controller');
+const router = express.Router();
+
+router.post('/upload', fileController.upload);
+router.post('/upload1', fileController.upload1);
+
+module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: File
+ *   description: File upload
+ */
+
+/**
+ * @swagger
+ * /file/upload:
+ *   post:
+ *     summary: upload a file
+ *     tags: [File]
+ *     requestBody:
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               fileName:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       "200":
+ *         description: Uploaded
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       "400":
+ *         description: Failed to upload
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ */
